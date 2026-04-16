530d8df9faf7b35c2c48b4ea2a1630e78b74fb5d
JoseHenrique442 <josealves809@gmail.com>
2026-04-16 11:11:54 -0300

    adiciona scripts `lint:check` e `lint_fix`

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # tabnews
+
 Tabnews - recriando o Tabnews do zero para o curso.dev
+
+# RC > Run Commands
+
+.nvmrc (nvm - node version management > run commands)
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Mensagem ajustada - 15/04/2026</h1>
+  return <h1>Mensagem ajustada - 15/04/2026</h1>;
 }
 
 export default Home;
