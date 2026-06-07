d3b2e32316f196a9eb4cdcd1c74c50d63f2ce137
José Henrique <josealves809@gmail.com>
2026-06-07 11:19:13 -0300

    test: cria testes para `users`

---
+++ tests/integration/api/v1/users/post.test.js
@@ -0,0 +1,34 @@
+import orchestrator from "tests/orchestrator.js";
+import database from "infra/database";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
+});
+
+describe("POST /api/v1/users", () => {
+  describe("Anonymous User", () => {
+    test("With unique and valid data", async () => {
+      await database.query({
+        text: "INSERT INTO users (username, email, password) VALUES ($1, $2, $3)",
+        values: ["josealves809", "josealves809@gmail.com", "senha123"],
+      });
+
+      await database.query({
+        text: "INSERT INTO users (username, email, password) VALUES ($1, $2, $3)",
+        values: ["josealves809-2", "Josealves809@gmail.com", "senha123"],
+      });
+
+      const users = await database.query("SELECT * FROM users");
+      console.log(users.rows)
+      const response = await fetch(
+        "http://localhost:3000/api/v1/users",
+        {
+          method: "POST",
+        },
+      );
+      expect(response.status).toBe(201);
+    });
+  });
+});
