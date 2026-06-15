17d5aca3a70a63cc8a96d57a3cb11ba808daccd0
José Henrique <josealves809@gmail.com>
2026-06-15 10:07:25 -0300

    feat: adiciona suporte ao método HTTP `PATCH` em `/api/v1/users/[username]`

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -80,7 +80,7 @@ describe("POST /api/v1/users", () => {
       expect(response2Body).toEqual({
         name: "ValidationError",
         message: "E-mail informado já está sendo utilizado.",
-        action: "Utilize outro e-mail para realizar o cadastro.",
+        action: "Utilize outro e-mail para realizar esta operação.",
         status_code: 400,
       });
     });
@@ -119,7 +119,7 @@ describe("POST /api/v1/users", () => {
       expect(response2Body).toEqual({
         name: "ValidationError",
         message: "Username informado já está sendo utilizado.",
-        action: "Utilize outro Username para realizar o cadastro.",
+        action: "Utilize outro Username para esta operação.",
         status_code: 400,
       });
     });
