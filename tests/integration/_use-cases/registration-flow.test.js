8d6c49822d24d705a8dbf86fe749c402aaa3b01a
José Henrique <josealves809@gmail.com>
2026-06-23 12:16:17 -0300

    feat: requisita `read:session` para acessar o endpoint /user

--- tests/integration/_use-cases/registration-flow.test.js
+++ tests/integration/_use-cases/registration-flow.test.js
@@ -11,99 +11,110 @@ beforeAll(async () => {
 });
 
 describe("Use case: Registration Flow (All successfful)", () => {
-  let createUserResponseBody;
-  let activationTokenId;
+  describe("Default user)", () => {
+    let createUserResponseBody;
+    let activationTokenId;
+    let createSessionsResponseBody;
 
-  test("Create user account", async () => {
-    const createUserResponse = await fetch(
-      "http://localhost:3000/api/v1/users",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
+    test("Create user account", async () => {
+      const createUserResponse = await fetch(
+        "http://localhost:3000/api/v1/users",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: "RegistrationFlow",
+            email: "registration-flow@gmail.com",
+            password: "registrationflowpassword",
+          }),
+        }
+      );
+
+      expect(createUserResponse.status).toBe(201);
+
+      createUserResponseBody = await createUserResponse.json();
+
+      expect(createUserResponseBody).toEqual({
+        id: createUserResponseBody.id,
+        username: "RegistrationFlow",
+        email: "registration-flow@gmail.com",
+        password: createUserResponseBody.password,
+        features: ["read:activation_token"],
+        created_at: createUserResponseBody.created_at,
+        updated_at: createUserResponseBody.updated_at,
+      });
+    });
+
+    test("Receive activation email", async () => {
+      const lastEmail = await orchestrator.getLastEmail();
+      expect(lastEmail.sender).toBe("<josealves809.1@gmail.com>");
+      expect(lastEmail.recipients[0]).toBe("<registration-flow@gmail.com>");
+      expect(lastEmail.subject).toBe("Ative seu cadastro!");
+      expect(lastEmail.text).toContain("RegistrationFlow");
+
+      activationTokenId = orchestrator.extractUUID(lastEmail.text);
+      expect(lastEmail.text).toContain(
+        `${webserver.origin}/cadastro/ativar/${activationTokenId}`
+      );
+
+      const activationTokenObject = await activation.findOneByValidId(activationTokenId);
+      expect(activationTokenObject.user_id).toBe(createUserResponseBody.id);
+      expect(activationTokenObject.used_at).toBe(null);
+    });
+
+    test("Activate account", async () => {
+      const activationResponse = await fetch(
+        `http://localhost:3000/api/v1/activations/${activationTokenId}`,
+        {
+          method: "PATCH",
         },
-        body: JSON.stringify({
-          username: "RegistrationFlow",
-          email: "registration-flow@gmail.com",
-          password: "registrationflowpassword",
-        }),
-      }
-    );
+      );
 
-    expect(createUserResponse.status).toBe(201);
+      expect(activationResponse.status).toBe(200);
 
-    createUserResponseBody = await createUserResponse.json();
+      const activationResponseBody = await activationResponse.json();
 
-    expect(createUserResponseBody).toEqual({
-      id: createUserResponseBody.id,
-      username: "RegistrationFlow",
-      email: "registration-flow@gmail.com",
-      password: createUserResponseBody.password,
-      features: ["read:activation_token"],
-      created_at: createUserResponseBody.created_at,
-      updated_at: createUserResponseBody.updated_at,
+      expect(Date.parse(activationResponseBody.used_at)).not.toBeNaN();
+
+      const activatedUser = await user.findOneByUsername("RegistrationFlow");
+      expect(activatedUser.features).toEqual(["create:session", "read:session"]);
+    });
+
+    test("Login", async () => {
+      const createSessionsResponse = await fetch(
+        "http://localhost:3000/api/v1/sessions",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: "registration-flow@gmail.com",
+            password: "registrationflowpassword",
+          }),
+        },
+      );
+
+      expect(createSessionsResponse.status).toBe(201);
+
+      createSessionsResponseBody = await createSessionsResponse.json();
+
+      expect(createSessionsResponseBody.user_id).toBe(createUserResponseBody.id);
+    });
+
+    test("Get User information", async () => {
+      const userResponse = await fetch("http://localhost:3000/api/v1/user", {
+        headers: {
+          cookie: `session_id=${createSessionsResponseBody.token}`
+        },
+      });
+      expect(userResponse.status).toBe(200);
+
+      const userResponseBody = await userResponse.json();
+      expect(userResponseBody.id).toBe(createUserResponseBody.id)
+
     });
   });
-
-  test("Receive activation email", async () => {
-    const lastEmail = await orchestrator.getLastEmail();
-    expect(lastEmail.sender).toBe("<josealves809.1@gmail.com>");
-    expect(lastEmail.recipients[0]).toBe("<registration-flow@gmail.com>");
-    expect(lastEmail.subject).toBe("Ative seu cadastro!");
-    expect(lastEmail.text).toContain("RegistrationFlow");
-
-    activationTokenId = orchestrator.extractUUID(lastEmail.text);
-    expect(lastEmail.text).toContain(
-      `${webserver.origin}/cadastro/ativar/${activationTokenId}`
-    );
-
-    const activationTokenObject = await activation.findOneByValidId(activationTokenId);
-    expect(activationTokenObject.user_id).toBe(createUserResponseBody.id);
-    expect(activationTokenObject.used_at).toBe(null);
-  });
-
-  test("Activate account", async () => {
-    const activationResponse = await fetch(
-      `http://localhost:3000/api/v1/activations/${activationTokenId}`,
-      {
-        method: "PATCH",
-      },
-    );
-
-    expect(activationResponse.status).toBe(200);
-
-    const activationResponseBody = await activationResponse.json();
-
-    expect(Date.parse(activationResponseBody.used_at)).not.toBeNaN();
-
-    const activatedUser = await user.findOneByUsername("RegistrationFlow");
-    expect(activatedUser.features).toEqual(["create:session"]);
-  });
-
-  test("Login", async () => {
-    const createSessionsResponse = await fetch(
-      "http://localhost:3000/api/v1/sessions",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email: "registration-flow@gmail.com",
-          password: "registrationflowpassword",
-        }),
-      },
-    );
-
-    expect(createSessionsResponse.status).toBe(201);
-
-    const createSessionsResponseBody = await createSessionsResponse.json();
-
-    expect(createSessionsResponseBody.user_id).toBe(createUserResponseBody.id);
-  });
-
-  test("Get User information", () => {
-
-  });
-
 });
--- tests/integration/api/v1/user/get.test.js
+++ tests/integration/api/v1/user/get.test.js
@@ -10,11 +10,31 @@ beforeAll(async () => {
 });
 
 describe("GET /api/v1/user", () => {
+  // 68defc
+  describe("Anonymous user)", () => {
+    test("Retrieving the endpoint", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/user");
+
+      expect(response.status).toBe(403);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        name: "ForbiddenError",
+        message: "Você não possui permissão para executar esta ação.",
+        action: 'Verifique se o seu usuário possui a feature "read:session".',
+        status_code: 403,
+      });
+    });
+  });
+
   describe("Default User", () => {
     test("With valid session", async () => {
       const createdUser = await orchestrator.createUser({
         username: "UserWithValidSession",
       });
+
+      const activatedUser = await orchestrator.activateUser(createdUser);
 
       const sessionObject = await orchestrator.createSession(createdUser.id);
 
@@ -37,9 +57,9 @@ describe("GET /api/v1/user", () => {
         username: "UserWithValidSession",
         email: createdUser.email,
         password: createdUser.password,
-        features: ["read:activation_token"],
+        features: ["create:session", "read:session"],
         created_at: createdUser.created_at.toISOString(),
-        updated_at: createdUser.updated_at.toISOString(),
+        updated_at: activatedUser.updated_at.toISOString(),
       });
       expect(uuidVersion(responseBody.id)).toBe(4);
       expect(Date.parse(responseBody.created_at)).not.toBeNaN();
@@ -156,6 +176,8 @@ describe("GET /api/v1/user", () => {
         username: "UserWithSession15Day",
       });
 
+      const activatedUser = await orchestrator.activateUser(createdUser)
+
       const sessionObject = await orchestrator.createSession(createdUser.id);
       jest.useRealTimers();
 
@@ -173,9 +195,9 @@ describe("GET /api/v1/user", () => {
         username: "UserWithSession15Day",
         email: createdUser.email,
         password: createdUser.password,
-        features: ["read:activation_token"],
+        features: ["create:session", "read:session"],
         created_at: createdUser.created_at.toISOString(),
-        updated_at: createdUser.updated_at.toISOString(),
+        updated_at: activatedUser.updated_at.toISOString(),
       });
       expect(uuidVersion(responseBody.id)).toBe(4);
       expect(Date.parse(responseBody.created_at)).not.toBeNaN();
