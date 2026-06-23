552b0853689e57d3486cb6851e200c0ee7171ded
José Henrique <josealves809@gmail.com>
2026-06-23 19:55:04 -0300

    feat: permite `update:user:other` atualizar outros usuários

--- tests/integration/api/v1/users/[username]/patch.test.js
+++ tests/integration/api/v1/users/[username]/patch.test.js
@@ -295,4 +295,45 @@ describe("PATCH /api/v1/users/[username]", () => {
       expect(incorrectPasswordMatch).toBe(false);
     });
   });
+
+  describe("Privileged User", () => {
+    test("With 'privileged user' targeting 'default user'", async () => {
+      const privilegedUser = await orchestrator.createUser();
+      const activatedPrivilegedUser = await orchestrator.activateUser(privilegedUser);
+      await orchestrator.addFeaturesToUser(privilegedUser, ["update:user:others"]);
+      const sessionObjectPrivilegedUser = await orchestrator.createSession(activatedPrivilegedUser.id);
+
+      const defaultUser = await orchestrator.createUser();
+
+
+      const response = await fetch(`http://localhost:3000/api/v1/users/${defaultUser.username}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Cookie: `session_id=${sessionObjectPrivilegedUser.token}`
+        },
+        body: JSON.stringify({
+          username: "QualquerCoisa",
+        }),
+      });
+      expect(response.status).toBe(200);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        id: defaultUser.id,
+        username: "QualquerCoisa",
+        email: defaultUser.email,
+        features: defaultUser.features,
+        password: responseBody.password,
+        created_at: responseBody.created_at,
+        updated_at: responseBody.updated_at,
+      });
+      expect(uuidVersion(responseBody.id)).toBe(4);
+      expect(Date.parse(responseBody.created_at)).not.toBeNaN();
+      expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+
+      expect(responseBody.updated_at > responseBody.created_at).toBe(true);
+    });
+  });
 });
