d809fd6e753bb8070bffdc871b2e549b8a74043a
José Henrique <josealves809@gmail.com>
2026-06-24 21:37:39 -0300

    refactor: ajusta para expect o e-mail ao atualizar o e-mail do usuário

--- tests/integration/api/v1/users/[username]/patch.test.js
+++ tests/integration/api/v1/users/[username]/patch.test.js
@@ -241,6 +241,9 @@ describe("PATCH /api/v1/users/[username]", () => {
       expect(Date.parse(responseBody.created_at)).not.toBeNaN();
       expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
       expect(responseBody.updated_at > responseBody.created_at).toBe(true);
+
+      const userInDatabase = await user.findOneByUsername(createdUser.username);
+      expect(userInDatabase.email).toBe("uniqueemail2@email.com");
     });
 
     test("With new 'password'", async () => {
